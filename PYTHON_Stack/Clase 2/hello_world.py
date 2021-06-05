6ac2d7d4fc83f41671745f0dcd762702fee66f82
jsilva16 <juansilva.ambiental@gmail.com>
2021-06-04 20:34:52 -0400

    hola-mundo

---
+++ PYTHON_Stack/Clase 2/hello_world.py
@@ -0,0 +1,23 @@
+# 1. TAREA: imprimir "Hola mundo"
+print("Hola mundo")
+# 2. imprimir "Hola Noelle!" con el nombre en una variable
+name = "Noelle"
+print("Hola",name,"!")	# con una coma
+print("Hola "+ name+"!")	# con un +
+# 3. imprimir "Hola 42!" con un numero en una variable
+name = 42
+print("Hola", name,"!")	# con una coma
+print("Hola "+ name+"!")	# con un + - !Este debería darnos un error!
+print("Hola %d!" %42)	
+# 4. imprimir "Me encanta comer sushi and pizza." con los alimentos en variables
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("Me encanta comer {} and {}.".format(fave_food1,fave_food2)) # con .format()
+print(f"Me encanta comer {fave_food1} and {fave_food2}.") # con una cadena f
+
+var = "test String"
+x = var.partition("String")
+print(x)
+
+y = var.swapcase()
+print(y)
---
+++ PYTHON_Stack/Clase 2/python_class2.py
@@ -0,0 +1,7 @@
+nombre = "Zen"
+print("Mi nombre es", nombre)
+print("mi nombre es {}".format(nombre))
+
+hw = "Hola %s" % "mundo" 	# con valores literales
+py = "Me encanta Python %d" % 3 
+print(hw, py)
